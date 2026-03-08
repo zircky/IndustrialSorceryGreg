97d476317eca44253e4c0b6e28339fecd1de4b70
ziran <gfgfgfggf02@gmail.com>
2026-03-08 20:26:28 +0300

    placeholder

--- src/main/java/com/zircky/industrialsorcerygreg/common/data/ISGPowerSubstationPlaceholder.java
+++ src/main/java/com/zircky/industrialsorcerygreg/common/data/ISGPowerSubstationPlaceholder.java
@@ -29,7 +29,7 @@ import java.util.List;
 {psInTicks}
 {psOutTicks}
 {psTimeToFillDrainText}
-{if {active} Work-Paused [WARNING: Machine is waiting.] }
+{isgActive}
 */
 
 public class ISGPowerSubstationPlaceholder {
